fe0ba12930ba19c105163599b5ad548ff8742446
Thefarmand <31881760+Thefarmand@users.noreply.github.com>
2018-10-08 18:09:35 +0200

    Test

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -9,33 +9,29 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            //Test for afvigelse på 0.001 gram skal blive grøn
-            var test1 = new Converter();
+            //Test for afvigelse på 0.001 gram skal blive grøn        
             Assert.AreEqual(28.34952, Converter.ToGram(1), 0.001);
         }
-        //Virker ikke endnu
+
         [TestMethod]
         public void TestMethod2()
         {
-            //Test for afvigelse over 0.003 gram. Skal fejle
-            var test2 = new Converter();
-            Assert.AreEqual(28.34952, Converter.ToGram(1), 0.6);
+            //Test for afvigelse på 0.001 gram skal blive grøn        
+            Assert.AreEqual(0.03527396195, Converter.ToOunce(1), 0.001);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            //Test for afvigelse over ounce. Skal fejle
-            var test3 = new Converter();
-            Assert.AreEqual(0.3527, Converter.ToOunce(1), 0.001);
+            //Test for negativt tal. Skal blive grøn
+            Assert.AreEqual(-28.34952, Converter.ToGram(-1), 0.001);
         }
 
         [TestMethod]
         public void TestMethod4()
         {
-            //Test for afvigelse over ounce. Skal fejle
-            var test4 = new Converter();
-            Assert.AreEqual(0.35279, Converter.ToOunce(1), 0.015);
+            //Test for negativt tal. Skal blive grøn
+            Assert.AreEqual(-0.03527396195, Converter.ToOunce(-1), 0.001);
         }
     }
 }
